da64b38df573d82b3d8f1c42e731df752d40ac80
Korben Kirscht <kirscht@gmail.com>
2021-01-08 18:21:22 -0800

    Initial Commit

--- www/shellshock_setup.sh
+++ www/shellshock_setup.sh
@@ -1,7 +1,27 @@
 #!/usr/bin/env bash
 
-cp /vagrant/index.html /var/www/.
+
 cp /vagrant/hello.cgi /usr/lib/cgi-bin/.
 
-chown root:root /var/www/index.html
 chown root:root /usr/lib/cgi-bin/hello.cgi
+
+mkdir -p /var/www/shellshock.galaxy3.net/
+cp /vagrant/index.html /var/www/shellshock.galaxy3.net/
+chown -R root:root /var/www/shellshock.galaxy3.net/
+
+cat <<__EOD__ >> /etc/apache2/sites-available/000_default
+</VirtualHost>
+<VirtualHost 10.55.56.56:443>
+		DocumentRoot /var/www/g3/
+		ServerName shellshock.galaxy3.net
+		DirectoryIndex index.html
+
+		SSLEngine On
+		SSLCertificateFile 		/etc/apache2/ssl/apache.crt
+		SSLCertificateKeyFile 	/etc/apache2/ssl/apache.key
+
+
+</VirtualHost>
+__EOD__
+
+apachectl reload
